45eff1b9089595b92130192c86b39ed89060eac4
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2017-10-09 08:46:58 +0200

    Documento ejecutable

---
+++ src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,16 @@
+
+package boletin2_1;
+
+public class Boletin2_1 {
+
+    public static void main(String[] args) {
+    
+        float base=4.5F, altura=3F,areaT;
+        
+        areaT=base*altura/2;
+        
+        System.out.println("area del rectangulo es: " + areaT);
+        
+    }
+    
+}
